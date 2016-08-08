e4efe7e9afec7dc1610d806215dd777a70b606ef
vitstol <vitalystokratnyi@outlook.com>
2016-08-08 17:13:55 +0300

    worked version

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,21 +1,28 @@
 package controllers
-import java.io.PrintWriter
+import java.io.{File, PrintWriter}
+import java.nio.charset.StandardCharsets
 
 import scala.sys.process._
 import com.google.inject.Inject
+import dispatch.url
 import play.api.libs.json._
 import play.api.mvc._
 import play.api.libs.ws.WSClient
-import net.liftweb.json._
+import play.api.mvc.MultipartFormData.FilePart
+import slack.api.SlackApiClient
+import slack.models.SlackFile
+import slack.api.BlockingSlackApiClient
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.postfixOps
 import scala.util.parsing.json.{JSONArray, JSONObject}
-import scala.concurrent.duration._
-import scala.concurrent.Future
+
 class Application @Inject() (ws: WSClient) extends Controller {
   val URL_SLACK_ROOM = "https://hooks.slack.com/services/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"
+  val URL_TELEGRAM_BOT_PHOTO = "https://api.telegram.org/bot261740146:AAHrUpO42RNdNZEyauchn1KTNI28xuWVToQ/sendPhoto"
+  val URL_TELEGRAM_BOT_MESSAGE= "https://api.telegram.org/bot261740146:AAHrUpO42RNdNZEyauchn1KTNI28xuWVToQ/sendMessage"
+
   val URL_SESSION_KEY = "http://testapi.monitorIT247.com/api/clientsessions"
   val PAYLOAD_FORARGUMENTS = Json.obj(
     "Name" -> "Eugene Malysh",
@@ -23,13 +30,13 @@ class Application @Inject() (ws: WSClient) extends Controller {
     "SessionType" -> "WordPressPlugin"
   )
 
-  def futureResponse(sessionKey:String) = {
+  def responseSlack(sessionKey:String) = {
     val url1 = "http://testapi.monitorIT247.com/api/monitors?sessionKey=" + sessionKey + "&id=557&idType=M"
     ws.url(url1).get().map { request =>
       val fullResponse:JsValue = Json.parse(request.body)
       val advicePrev = fullResponse \\ "advice"
-      val advice = advicePrev.head.toString().replace("<p>","")
-        .replace("</p>","")
+      val advice = advicePrev.head.toString().replace("<p>","_")
+        .replace("</p>","_")
         .replace("<span style=font-size: 18px; font-family: arial; color: #2e75b6;>","")
         .replace("</span>","")
         .replace("<span style=font-size: 18px; font-family: arial; color: black;>","•")
@@ -39,18 +46,45 @@ class Application @Inject() (ws: WSClient) extends Controller {
         .replace("\"","")
         .replace("\\r","\r")
         .replace("\\n","\n")
-        .replace("Advice","Advice")
-      val Payload = Json.obj(
-        "channel"->"@vitaliys",
+        .replace("Advice(please take note)","*_`Advice(please take note)`_*")
+        .replace("•Note","Note")
+        .replace("•nominated limit of","_nominated limit of_")
+      val PAYLOAD_SLACK = Json.obj(
+        "bot_access_token"->"xoxb-67157110468-fWjRy6P6iaboxBdK8LOZOmLF",
+        "channel"->"#random",git
         "username"->"webhookbot",
         "mrkdwn" ->true,
-        "token"->"files:write:user",
-        "ok"-> true,
-        "file"->"chart.png"
+        "text"->advice
       )
+      ws.url(URL_SLACK_ROOM).post(PAYLOAD_SLACK)
+    }
+  }
 
-
-      ws.url(URL_SLACK_ROOM).post(Payload)
+  def responseTelegram(sessionKey:String) = {
+    val url1 = "http://testapi.monitorIT247.com/api/monitors?sessionKey=" + sessionKey + "&id=557&idType=M"
+    ws.url(url1).get().map { request =>
+      val fullResponse:JsValue = Json.parse(request.body)
+      val advicePrev = fullResponse \\ "advice"
+      val advice = advicePrev.head.toString().replace("<p>","_")
+        .replace("</p>","_")
+        .replace("<span style=font-size: 18px; font-family: arial; color: #2e75b6;>","")
+        .replace("</span>","")
+        .replace("<span style=font-size: 18px; font-family: arial; color: black;>","•")
+        .replace("<span style=font-size: 18px; font-family: arial; color: red;>","")
+        .replace("<ul>","").replace("</ul>","")
+        .replace("<li>","                             ").replace("</li>","")
+        .replace("\"","")
+        .replace("\\r","\r")
+        .replace("\\n","\n")
+        .replace("Advice(please take note)","_`Advice(please take note)`_")
+        .replace("•Note","Note")
+        .replace("•nominated limit of","_nominated limit of_")
+      val PAYLOAD= Json.obj(
+        "chat_id"->"39404509",
+        "text"->advice,
+        "parse_mode"->"Markdown"
+      )
+      ws.url(URL_TELEGRAM_BOT_MESSAGE).post(PAYLOAD)
     }
   }
 
@@ -98,16 +132,30 @@ class Application @Inject() (ws: WSClient) extends Controller {
     }
   }
 
+  def uploadImageToSlack = {
+    val term = "curl -F file=@chart.png -F channels=#random -F token=xoxb-67157110468-fWjRy6P6iaboxBdK8LOZOmLF  https://slack.com/api/files.upload"
+    val uploadFile = term.!
+    uploadFile
+  }
+  def uploadImageToTelegram = {
+    val term = "curl -s -X POST https://api.telegram.org/bot261740146:AAHrUpO42RNdNZEyauchn1KTNI28xuWVToQ/sendPhoto -F chat_id=39404509 -F photo=@chart.png"
+    val uploadFile = term.!
+    uploadFile
+  }
+
+
   def getArguments = Action.async {
     ws.url(URL_SESSION_KEY).post(PAYLOAD_FORARGUMENTS).map { request =>
       val parts: Array[String] = request.body.split(",")
       val sessionKey: Option[String] = parts.find(_.contains("SessionKey"))
       mergeJson(sessionKey.getOrElse("").substring(14, 50))
-      futureResponse(sessionKey.getOrElse("").substring(14, 50))
+      responseSlack(sessionKey.getOrElse("").substring(14, 50))
+      uploadImageToSlack
+      responseTelegram(sessionKey.getOrElse("").substring(14, 50))
+      uploadImageToTelegram
       Ok("")
     }
   }
-
-
 }
-//Await.result(mergeJson(sessionKey.getOrElse("").substring(14, 50)), 1 seconds)
+
+
--- build.sbt
+++ build.sbt
@@ -9,7 +9,8 @@ scalaVersion := "2.11.7"
 libraryDependencies ++= Seq( jdbc , cache , ws   , specs2 % Test,
   "com.typesafe.play" % "play-json_2.11" % "2.4.2",
    "org.scalaj" % "scalaj-http_2.11" % "2.3.0",
-  "net.liftweb" %% "lift-json" % "2.6"
+  "net.liftweb" %% "lift-json" % "2.6",
+  "com.github.gilbertw1" %% "slack-scala-client" % "0.1.7"
 )
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
